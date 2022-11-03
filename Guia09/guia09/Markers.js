6de4842ee730a5b0908df25d1837a4292bb3b440
susan749 <60519933+susan749@users.noreply.github.com>
2022-11-03 16:01:17 -0600

    guia09

---
+++ Guia09/guia09/Markers.js
@@ -0,0 +1,42 @@
+export const markers = [
+    {
+    coordinate: {
+    latitude: 13.4945688,
+    longitude: -89.3868631,
+},
+title: 'Playa el Tunco',
+description: 'Playa el Tunco en la Libertad',
+image:
+'https://lh3.googleusercontent.com/p/AF1QipPM0zAupIxVZe7R6YSgPpt-ffLAATrxS2yN70i=s1600-h1836',
+},
+{
+coordinate: {
+latitude: 13.1698526,
+longitude: -88.3076309,
+},
+title: 'Playa El Espino',
+description: 'Playa El Espino Usulután',
+image:
+'https://cdn-pro.elsalvador.com/wp-content/uploads/2018/01/El-Espino.jpg',
+},
+{
+coordinate: {
+latitude: 13.4924991,
+longitude: -89.409454,
+},
+title: 'Playa el Sunzal',
+description: 'Playa el Sunzal La Libertad',
+image:
+'https://wikisivar.com/wp-content/uploads/2018/12/Surfistas-en-playa-enSunzal.jpg',
+},
+{
+coordinate: {
+latitude: 13.5249991,
+longitude: -89.8141763,
+},
+title: 'Playa los Cóbanos',
+description: 'Playa los cóbanos Sonsonate',
+image:
+'https://wikisivar.com/wp-content/uploads/2018/12/Playa-los-CobanosTurismo.jpg',
+},
+];
